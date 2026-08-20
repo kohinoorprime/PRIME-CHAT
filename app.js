5fc054e30f4cd415da8a80fde3c90108f9440b95
kohinoorprime <kohinoorprime.x@gmail.com>
2026-08-20 23:13:23 +0530

    Add event listeners for login and signup buttons

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
 
+loginBtn.addEventListener("click", function () {
+  alert("PRIME CHAT Login button working!");
+});
+
+signupBtn.addEventListener("click", function () {
+  alert("PRIME CHAT Signup button working!");
+});
